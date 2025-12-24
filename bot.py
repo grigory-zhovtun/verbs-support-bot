d35fb0e52ccffc6eecf6437c61e697ab575ef369
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2025-12-24 22:10:30 +0500

    Add a basic Telegram bot implementation using python-telegram-bot library

---
+++ bot.py
@@ -0,0 +1,28 @@
+import logging
+import os
+import dotenv
+from telegram import Update
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text("Здравствуйте!")
+
+
+async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(update.message.text)
+
+
+def main():
+    dotenv.load_dotenv()
+    token = os.getenv("TG_TOKEN")
+
+    app = ApplicationBuilder().token(token).build()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+
+    app.run_polling()  # Сам создаёт event loop и управляет всем
+
+
+if __name__ == '__main__':
+    main()
