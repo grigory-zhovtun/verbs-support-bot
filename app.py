39b5f8cac6c4abc09d3b40438594576d222e50f2
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-13 23:32:36 +0500

    setup DialogFlow

--- app.py
+++ app.py
@@ -13,23 +13,12 @@ def fetch_intent_texts(project_id, session_id, query_input):
             request={"session": session, "query_input": query_input}
     )
 
-def detect_intent(project_id, session_id, texts, language_code):
-    for text in texts:
-        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+def get_dialogflow_response(project_id, session_id, text, language_code):
+    text_input = dialogflow.TextInput(text=text, language_code=language_code)
+    query_input = dialogflow.QueryInput(text=text_input)
+    response = fetch_intent_texts(project_id, session_id, query_input)
 
-        query_input = dialogflow.QueryInput(text=text_input)
-
-        response = fetch_intent_texts(project_id, session_id, query_input)
-
-        print("=" * 20)
-        print(f"Query text: {response.query_result.query_text}")
-        print(
-            f"Detected intent: "
-            f"{response.query_result.intent.display_name} "
-            f"(confidence: {response.query_result.intent_detection_confidence})\n"
-            )
-
-        print(f"Fulfillment text: {response.query_result.fulfillment_text}\n")
+    return response.query_result.fulfillment_text
 
 
 if __name__ == '__main__':
@@ -37,15 +26,15 @@ if __name__ == '__main__':
     project_id = os.getenv("PROJECT_ID")
 
     parser = argparse.ArgumentParser(description='Dialogflow intent detection')
-    parser.add_argument('texts', nargs='+', help='Texts to detect intent for')
+    parser.add_argument('text', nargs='+', help='Texts to detect intent for')
     parser.add_argument('--session_id', default='123456789', help='Session ID (default: 123456789)')
     parser.add_argument('--language', default='ru-RU', help='Language code (default: ru-RU)')
 
     args = parser.parse_args()
 
-    detect_intent(
+    get_dialogflow_response(
         project_id,
         args.session_id,
-        args.texts,
+        args.text,
         args.language
     )
--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import os
 import dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
+from app import get_dialogflow_response
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -10,14 +11,21 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(update.message.text)
+    chat_id = update.message.chat_id
+    text = update.message.text
+    project_id = context.bot_data['project_id']
+    dialogflow_response = get_dialogflow_response(project_id, str(chat_id), text, 'ru-RU')
+
+    await update.message.reply_text(dialogflow_response)
 
 
 def main():
     dotenv.load_dotenv()
     token = os.getenv("TG_TOKEN")
+    project_id = os.getenv("PROJECT_ID")
 
     app = ApplicationBuilder().token(token).build()
+    app.bot_data['project_id'] = project_id
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
 
