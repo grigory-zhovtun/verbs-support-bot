2eec317553407e4525e51dfe7c7c5c7d41c795fc
Grigory Zhovtun <grigorii.zhovtun@gmail.com>
2026-01-17 17:26:19 +0500

    Improve error handling in Telegram notifications: check JSON response for errors and raise exceptions if necessary.

--- error_handler.py
+++ error_handler.py
@@ -12,5 +12,10 @@ def send_error_to_telegram(
     text = f"🚨🚨🚨 Ошибка в {bot_name}:\n\n{error_message} 🚨🚨🚨"
 
     url = f"https://api.telegram.org/bot{tg_token}/sendMessage"
-    requests.post(url, data={"chat_id": admin_chat_id, "text": text})
+    response = requests.post(url, data={"chat_id": admin_chat_id, "text": text})
+    response.raise_for_status()
 
+    # телега иногда возвращает 200, но с ошибкой в JSON
+    decoded_response = response.json()
+    if 'error' in decoded_response:
+        raise requests.exceptions.HTTPError(decoded_response['error'])
